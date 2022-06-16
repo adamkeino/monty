3de89c98488457edae2adbaad1e6f58f6a02144a
Adam Keino <keinoadam@gmail.com>
2022-06-16 15:24:38 +0000

    swap

--- monty.h
+++ monty.h
@@ -69,6 +69,7 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **doubly, unsigned int cline);
 void _pop(stack_t **doubly, unsigned int cline);
+void _swap(stack_t **doubly, unsigned int cline);
 
 /*get function*/
 void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
