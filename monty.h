012faeeb6beb8cec344bef3701edc757f88a9f3c
Adam Keino <keinoadam@gmail.com>
2022-06-16 15:00:50 +0000

    pop

--- monty.h
+++ monty.h
@@ -68,6 +68,7 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **doubly, unsigned int cline);
+void _pop(stack_t **doubly, unsigned int cline);
 
 /*get function*/
 void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
